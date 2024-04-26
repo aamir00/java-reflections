d8461406068970afcbd07a9eafe26a7c249999af
Amair Khan <mohammed.khan@wavemaker.com>
2024-04-26 13:09:01 +0530

    first commit

---
+++ src/main/java/com/javademos/reflection/MainT.java
@@ -0,0 +1,14 @@
+package com.javademos.reflection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainT {
+
+    public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(MainT.class);
+        logger.info("asfdasdfadfafsd");
+        logger.info("asdf");
+    }
+}
